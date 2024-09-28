89580bdf9ab5cf4a50a37c5dcccfc23ba5824cd3
hungrymissilepod <46930094+hungrymissilepod@users.noreply.github.com>
2024-09-28 12:57:38 +0100

    Initial commit: Create app

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      theme: CupertinoThemeData(
+          textTheme: CupertinoTextThemeData(
+              textStyle: TextStyle(
+        fontSize: 14,
+        color: CupertinoColors.black,
+      ))),
+      title: 'Flutter Demo',
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Counter App'),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: CupertinoTheme.of(context).textTheme.textStyle,
+            ),
+            Center(
+              child: CupertinoButton.filled(
+                  child: Icon(CupertinoIcons.add),
+                  onPressed: () {
+                    _incrementCounter();
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
