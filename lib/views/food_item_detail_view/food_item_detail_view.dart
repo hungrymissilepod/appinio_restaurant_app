1a5d114c1828ac041981e061bb150a6c94a495c5
hungrymissilepod <46930094+hungrymissilepod@users.noreply.github.com>
2024-10-02 18:55:19 +0100

    fix: Fill whole screen in FoodItemDetailView

--- lib/views/food_item_detail_view/food_item_detail_view.dart
+++ lib/views/food_item_detail_view/food_item_detail_view.dart
@@ -24,62 +24,65 @@ class FoodItemDetailView extends StatelessWidget {
         ),
       ),
       child: SafeArea(
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(
-              parent: AlwaysScrollableScrollPhysics()),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 200,
-                child: FoodImage(imageUrl: item.imageUrl),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Flexible(
-                          child: Text(
-                            '${item.name}',
-                            maxLines: 1,
-                            style: CupertinoTheme.of(context)
-                                .textTheme
-                                .textStyle
-                                .copyWith(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 24,
-                                ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      '${item.price}',
-                      style: CupertinoTheme.of(context)
-                          .textTheme
-                          .textStyle
-                          .copyWith(fontSize: 18),
-                    ),
-                    const SizedBox(height: 32),
-                    FoodInfoSection(
-                      title: 'Description',
-                      body: item.description ?? '',
-                    ),
-                    const CommonDivider(),
-                    FoodInfoSection(
-                      title: 'Ingredients',
-                      body: item.ingredients ?? '',
-                    ),
-                  ],
+        child: SizedBox(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics()),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 200,
+                  child: FoodImage(imageUrl: item.imageUrl),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            child: Text(
+                              '${item.name}',
+                              maxLines: 1,
+                              style: CupertinoTheme.of(context)
+                                  .textTheme
+                                  .textStyle
+                                  .copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 24,
+                                  ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        '${item.price}',
+                        style: CupertinoTheme.of(context)
+                            .textTheme
+                            .textStyle
+                            .copyWith(fontSize: 18),
+                      ),
+                      const SizedBox(height: 32),
+                      FoodInfoSection(
+                        title: 'Description',
+                        body: item.description ?? '',
+                      ),
+                      const CommonDivider(),
+                      FoodInfoSection(
+                        title: 'Ingredients',
+                        body: item.ingredients ?? '',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
